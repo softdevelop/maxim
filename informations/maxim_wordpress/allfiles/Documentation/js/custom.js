e5fa6e8cc886b20e5d834794a6008fcb24f8c044
unknown <antone.bkdn@gmail.com>
2014-09-27 12:18:19 +0700

    update fix same demo site

--- informations/maxim_wordpress/allfiles/Documentation/js/custom.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($) {
-	$('.toggle_handle').on('click', function() {
-		$(this).next().toggleClass('toggle');
-	})
-});
--- informations/maxim_wordpress/allfiles/maxim1.2/sidebar.php
+++
@@ -1,5 +0,0 @@
-<div id="sidebar" class="widgets-area">
-	<ul>
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>
-	</ul>
-</div>
